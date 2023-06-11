964583a4becac67893a9bb451744842b17027253
ULISESROSALESALEJOS <urosales507@gmail.com>
2023-06-11 19:07:54 -0400

    realizando la migrations y definiendo tablas en la base de datos

---
+++ tienda_animal/clientes/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Cliente,Genero
+
+# Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Genero)
---
+++ tienda_animal/productos/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    id_categoria = models.AutoField(db_column='idCategoria', primary_key=True) 
+    categoria    = models.CharField(max_length=30, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.categoria)
+    
+class Producto(models.Model):
+    id_producto      = models.IntegerField(primary_key=True)
+    nombre           = models.CharField(max_length=50)
+    precio           = models.IntegerField()
+    stock            = models.IntegerField()
+    descripcion      = models.CharField(max_length=150,blank=False, null=False) 
+    id_categoria     = models.ForeignKey('Categoria',on_delete=models.CASCADE, db_column='idCategoria')
+
+    def __str__(self):
+        return str(self.nombre)
